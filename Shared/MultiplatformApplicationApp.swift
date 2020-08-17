6e3b7b61a8f1b34c08eb48c729606ddc64d3e3ea
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-08-17 20:58:02 +0300

    Initial Commit

---
+++ Shared/MultiplatformApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplatformApplicationApp.swift
+//  Shared
+//
+//  Created by Ramill Ibragimov on 17.08.2020.
+//
+
+import SwiftUI
+
+@main
+struct MultiplatformApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
