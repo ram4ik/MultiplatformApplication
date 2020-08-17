33ef85e5b58a0703f05757047af52cdbedcc88a9
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-08-17 21:27:56 +0300

    project init

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,13 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .leading, spacing: 4.0) {
+            Spacer()
+            HStack {
+                Spacer()
+                Image(systemName: "person.icloud.fill")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .foregroundColor(.white)
+                Spacer()
+            }
+            Text("SwiftUI")
+                .fontWeight(.bold)
+                .foregroundColor(.white)
+            Text("Example")
+                .font(.footnote)
+                .foregroundColor(.white)
+        }
+        .padding(.all)
+        .background(Color.blue, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+        .cornerRadius(20.0)
+        .shadow(radius: 10)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+            ContentView()
+                .preferredColorScheme(.dark)
+        }
     }
 }
